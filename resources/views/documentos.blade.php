a38e7eb1232f7b198145db9130a69d27521d904d
alvaro aguirre <alguirre2@gmail.com>
2024-12-10 03:52:20 -0600

    laravel docuemntos

---
+++ resources/views/documentos.blade.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Card.css">
+    <style>
+
+        .targeta {
+    border: 2px solid gray;
+    width: 15%;
+    height: 150px;
+    display: inline;
+        }
+
+.padded{
+    padding: 20px 25px;
+    margin-top:20px
+  
+}
+
+.flexbox{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+
+}
+
+    </style>
+    <title>documentos elaborables</title>
+</head>
+<body>
+    <div class= "flexbox">
+
+        <div class="targeta padded ">
+            <div class=imagen>
+                <img src="" alt="nombre alterno" ></img>
+            </div>
+            <h1 class="titulo">
+                <!-- aqui ira la info de nombre de la base de datos -->
+                hola
+            </h1>
+            <button class="buttom"><a href="">
+                ir
+            </a>
+        </button>
+        </div>
+        <div class="targeta padded ">
+            <div class=imagen>
+                <img src="" alt="nombre alterno" ></img>
+            </div>
+            <h1 class="titulo">
+                
+                <!-- aqui ira la info de nombre de la base de datos -->
+                nombre 2
+            </h1>
+            <button class="buttom"><a href="">
+
+                ir
+            </a>
+        </button>
+        </div>
+        <div class="targeta padded ">
+            <div class=imagen>
+                <img src="" alt="nombre alterno" ></img>
+            </div>
+            <h1 class="titulo">
+                
+                <!-- aqui ira la info de nombre de la base de datos -->
+                nombre 3
+            </h1>
+            <button class="buttom"><a href="">
+
+                ir
+            </a>
+        </button>
+        </div>
+        <div class="targeta padded ">
+            <div class=imagen>
+                <img src="" alt="nombre alterno" ></img>
+            </div>
+            <h1 class="titulo">
+                
+                <!-- aqui ira la info de nombre de la base de datos -->
+                nombre 4
+            </h1>
+            <button class="buttom"><a href="">
+
+                ir
+            </a>
+        </button>
+        </div>
+        <div class="targeta padded ">
+            <div class=imagen>
+                <img src="" alt="nombre alterno" ></img>
+            </div>
+            <h1 class="titulo">
+                
+                <!-- aqui ira la info de nombre de la base de datos -->
+                nombre 5
+            </h1>
+            <button class="buttom"><a href="">
+
+                ir
+            </a>
+        </button>
+        </div>
+        <div class="targeta padded ">
+            <div class=imagen>
+                <img src="" alt="nombre alterno" ></img>
+            </div>
+            <h1 class="titulo">
+                
+                <!-- aqui ira la info de nombre de la base de datos -->
+                nombre 6
+            </h1>
+            <button class="buttom"><a href="">
+
+                ir
+            </a>
+        </button>
+        </div>
+        
+    </div>
+     
+        
+    </div>
+</body>
+</html>
